89738408a394ac904314299af6cd6b974053ec1a
Josec0Loc0 <130604593+Josec0Loc0@users.noreply.github.com>
2023-05-03 15:26:58 -0500

    Commit3final Progreso1

---
+++ Commit3final.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, suma = 0, contador = 0; // aqui se declaran las variables n para almacenar cada número ingresado,haciendo posible la suma de los números negativos.
+    while (abs(suma) <= 1200) { // se define que mientras la suma de los numeros negativos sea menor o igual a 1200
+        printf("Ingrese un numero: ");
+        scanf("%d", &n); // Ingreso de números
+        suma += n < 0 ? n : 0; // suma de todos los números negativos
+        contador++; // aumentando el contador de la suma de los números negativos
+    }
+    printf("Se ingresaron %d numeros para que la suma de los numeros negativos de en valor absoluto mayor que 1200", contador); // esta linea imprime el resultado
+    return 0;
+}
