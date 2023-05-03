066eb0ad428de7df62b38044c9716cdb225683ad
Josec0Loc0 <130604593+Josec0Loc0@users.noreply.github.com>
2023-05-03 15:00:53 -0500

    Create Commit1.c

---
+++ Commit1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Libreria de entrada y salida la cual contiene los principales comandos como puede ser printf, scanf, etc.
+#include <stdlib.h> // Esta libreria contiene los prototipos de funciones para gestión de memoria dinámica
+
+int main()
+{
+
+    return 0;
+}
